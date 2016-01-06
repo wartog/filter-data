2a2b712610f549c9cd80995255870bd33fff82f5
Diogo Lundberg <diogo@lundberg.com.br>
2016-01-06 11:32:05 -0200

    Comments and Usage example.

--- filter.py
+++ filter.py
@@ -2,21 +2,29 @@ import sys, os, click
 from _to_df import to_df
 
 
+'''
+USAGE EXAMPLE:
+
+python filter.py data/BH_2011.csv data/2011
+
+'''
+
 @click.command()
 @click.argument('file_path', type=click.Path(exists=True), required=True)
 @click.argument('output_path', type=click.Path(), required=True)
 def main(file_path, output_path):
 
+    # This should create the folder if it's not there yet
     if not os.path.exists(output_path): os.makedirs(output_path)
 
+    # Reading the csv, getting a dataframe from pd.read_csv
     df = to_df(sys.argv[1])
+
+    # Filtering data, where Municipality_ID column = 310620
     df = df[(df.Municipality_ID == 310620)]
 
-
-    import pdb; pdb.set_trace()
+    # Write output
     new_file_path = os.path.abspath(os.path.join(output_path, "fout.csv"))
-    print new_file_path
-
     df.to_csv(open(new_file_path, 'wb+'), sep=";", index=True, float_format="%.3f")
 
 if __name__ == "__main__":
