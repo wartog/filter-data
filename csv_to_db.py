c6c00c55ca7c8e4001ec5e8cb617d66b8e61a72a
Diogo Lundberg <diogo@lundberg.com.br>
2016-01-07 18:39:26 -0200

    Renaming importing example

---
+++ csv_to_db.py
@@ -0,0 +1,22 @@
+import click
+import os
+
+'''
+USAGE:
+python import_to_db.py -file_path=data/2015/fout.csv -table_name=tabela_test
+'''
+
+@click.command()
+@click.option('-file_path', type=click.Path(exists=True), prompt=True, help='Csv input file.')
+@click.option('-table_name', prompt=True, help='Name of table in database eg test.')
+def main(file_path, table_name):
+    print "importing", file_path, "into", table_name
+
+
+    # Only possible with local-infile=1 in my.conf
+    cmd = '''mysql --local-infile $DB_NAME -e "LOAD DATA LOCAL INFILE '%s' INTO TABLE %s FIELDS TERMINATED BY ';' LINES TERMINATED BY '\\n' IGNORE 1 LINES; "''' % (file_path, table_name)
+    print cmd
+    os.system(cmd)
+
+if __name__ == '__main__':
+    main()
