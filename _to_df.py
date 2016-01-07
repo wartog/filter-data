e5dd82638fed766336a70cb1523b51be2ce85730
Diogo Lundberg <diogo@lundberg.com.br>
2016-01-07 18:39:48 -0200

    Adding utf-8 for reading.

--- _to_df.py
+++ _to_df.py
@@ -3,11 +3,11 @@ import pandas as pandas
 
 from helpers import get_file
 
-def to_df(input_file_path, index=False):
+def to_df(input_file_path, separator=";", index=False):
     input_file = get_file(input_file_path)
     s = time.time()
 
-    df = pandas.read_csv(input_file, sep="|", engine='c', decimal=',')
+    df = pandas.read_csv(input_file, sep=separator, engine='c', decimal=',', encoding='utf-8')
 
     print (time.time() - s) / 60.0, "minutes to read."
 
